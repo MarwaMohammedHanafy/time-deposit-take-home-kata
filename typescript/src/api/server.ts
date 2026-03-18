e2ea69d6a62d6e2b0ab552775635005d9dcc2c0d
Marwa <marwama3bade@gmail.com>
2026-03-18 00:33:47 +0000

    feat: implement GET /time-deposits endpoint

--- typescript/src/api/server.ts
+++ typescript/src/api/server.ts
@@ -1,7 +1,17 @@
 import express from 'express'
+import { GetTimeDepositsUseCase } from '../application/GetTimeDepositsUseCase'
+import { PrismaTimeDepositRepository } from '../infrastructure/PrismaTimeDepositRepository'
 
 const app = express()
 
 app.use(express.json())
+
+const repository = new PrismaTimeDepositRepository()
+const getTimeDepositsUseCase = new GetTimeDepositsUseCase(repository)
+
+app.get('/time-deposits', async (_req, res) => {
+  const deposits = await getTimeDepositsUseCase.execute()
+  res.json(deposits)
+})
 
 export { app }
